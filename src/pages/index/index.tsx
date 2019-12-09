0143b78241f7c6170d45c533ac3a73e0b8d040f0
Jay Fong <fjc0kb@gmail.com>
2019-12-09 22:07:36 +0800

    feat: 简化 styles

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import { View, Text, Image } from 'remax/wechat';
-import styles from './index.module.css';
+import _ from './index.module.css';
 
 export default () => {
   return (
-    <View className={styles.app}>
-      <View className={styles.header}>
+    <View className={_.app}>
+      <View className={_.header}>
         <Image
           src="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ"
-          className={styles.logo}
+          className={_.logo}
         />
-        <View className={styles.text}>
-          编辑 <Text className={styles.path}>src/pages/index/index.tsx</Text> 开始
+        <View className={_.text}>
+          编辑 <Text className={_.path}>src/pages/index/index.tsx</Text> 开始
         </View>
       </View>
     </View>
